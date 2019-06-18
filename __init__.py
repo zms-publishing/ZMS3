0a437540fbff12f805a4131ac1e6b99be58a336c
zmsdev <zmsdev@localhost>
2019-06-18 15:15:30 +0000

    repository: remove acquired and support portal-clients
    svn path=/ZMS/trunk/; revision=5098

--- __init__.py
+++ __init__.py
@@ -50,7 +50,7 @@ import ZMSZCatalogAdapter
 import ZMSFormatProvider, ZMSFormatProviderAcquired
 import ZMSMetacmdProvider, ZMSMetacmdProviderAcquired
 import ZMSWorkflowProvider, ZMSWorkflowProviderAcquired
-import ZMSRepositoryManager, ZMSRepositoryManagerAcquired
+import ZMSRepositoryManager
 
 try:
   from Products.CMFCore.DirectoryView import registerFileExtension
